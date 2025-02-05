f24fa9c4a8f5a0b09086698e668c17627ce779ae
Anika Patel <76812213+Anika-del@users.noreply.github.com>
2025-02-05 22:48:49 +0530

    project

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import LogInOut from "./conditionRendr/LogInOut"; // Ensure the path matches the actual directory structure
+//import LogInOut from "./conditionRendr/LogInOut"; // Ensure the path matches the actual directory structure
 //import LogInOut2 from "./conditionRendr/LogInOut2";
+import Counter from "./useReducer/Counter"
+
 function App() {
   return (
     <>
-      <LogInOut />
+      {/* <LogInOut /> */}
       {/* <LogInOut2 /> */}
+      <Counter />
     </>
   );
 }
