c4ba5e22c20a3f9e763a6ebd41f86c723bf61f75
Anika Patel <76812213+Anika-del@users.noreply.github.com>
2025-02-11 22:22:27 +0530

    router

--- learning-routes/src/App.jsx
+++ learning-routes/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Service from './pages/Service';
+import Contact from './pages/Contact';
 import './Style.css';
 
 const App = () => {
@@ -15,11 +17,19 @@ const App = () => {
           <li>
             <Link to="/about">About</Link>
           </li>
+          <li>
+            <Link to="/service">Service</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
